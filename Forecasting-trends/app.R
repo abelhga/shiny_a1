e00ab784da0a5f27626aad8c01b1b5120c18e8da
Abel Hernández García <abelhernandez@MacBookPro.lan>
2024-08-28 12:24:53 +0100

    Uploading the first version of the shiny app

---
+++ Forecasting-trends/app.R
@@ -0,0 +1,103 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(shiny)
+library(dplyr)
+library(ggplot2)
+library(gtrendsR)
+library(prophet)
+library(lubridate)
+
+
+# Define UI for application that draws a histogram
+
+ui <- fluidPage(
+  titlePanel("Google Trends Forecasting App"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      textInput("keywords", "Enter Keywords (comma-separated):", "laptop,desktop,tablet"),
+      selectInput("geo", "Select Region:", choices = c("GB", "US","MX","Worldwide"), selected = "GB"),
+      sliderInput("forecast_period", "Forecast Period (Days):", min = 1, max = 730, value = 180),
+      actionButton("update", "Update Forecast")
+    ),
+    
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Trends Plot", plotOutput("trendsPlot")),
+        tabPanel("Forecast Plot", plotOutput("forecastPlot")),
+        tabPanel("Model Components", plotOutput("componentsPlot"))
+      )
+    )
+  )
+)
+
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  # Reactive data retrieval based on user inputs
+  trends_data <- eventReactive(input$update, {
+    gtrends(
+      keyword = unlist(strsplit(input$keywords, ",")),
+      geo = input$geo,
+      time = "today+5-y",
+      tz = 0
+    )$interest_over_time %>%
+      mutate(date = ymd(date)) %>%
+      filter(date < Sys.Date() - 1)
+  })
+  
+  # Reactive forecasting model
+  forecast_data <- eventReactive(input$update, {
+    laptop_data <- trends_data() %>%
+      filter(keyword == unlist(strsplit(input$keywords, ","))[1]) %>%
+      select(date, hits) %>%
+      rename(ds = date, y = hits) %>%
+      arrange(ds)
+    
+    m <- prophet(laptop_data)
+    future <- make_future_dataframe(m, periods = input$forecast_period, freq = "day", include_history = TRUE)
+    predict(m, future)
+  })
+  
+  # Plot trends data
+  output$trendsPlot <- renderPlot({
+    trends_data() %>%
+      ggplot() +
+      geom_line(aes(date, hits, color = keyword), size = 0.5) +
+      scale_y_continuous(limits = c(0, 100)) +
+      scale_color_brewer(palette = 'Set1') +
+      theme_minimal() +
+      labs(x = NULL, y = "Relative Search Interest",
+           title = 'Google Trends: interest over time',
+           caption = "Data from Google Trends")
+  })
+  
+  # Plot forecast data
+  output$forecastPlot <- renderPlot({
+    forecast_data() %>%
+      ggplot() +
+      geom_line(aes(ds, yhat)) +
+      geom_ribbon(aes(ds, ymin = yhat_lower, ymax = yhat_upper), fill = 'green', alpha = 0.3) +
+      theme_bw() +
+      labs(x = NULL, y = "Relative Search Interest",
+           title = "Forecasting with Prophet")
+  })
+  
+  # Plot forecast components
+  output$componentsPlot <- renderPlot({
+    prophet_plot_components(prophet(trends_data()), forecast_data())
+  })
+}
+
+
+# Run the application 
+shinyApp(ui = ui, server = server)
